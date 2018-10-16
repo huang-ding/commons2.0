b8dfd59fdf32120f2618134192f80eaf418fe04a
huangdingQN <huangding@qnvip.com>
2018-10-16 15:06:18 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,15 @@
+/**
+ * @author huangding
+ * @description
+ * @date 2018/10/16 15:03
+ */
+public class Test {
+
+    static {
+        System.out.printf("121");
+    }
+
+    public static void main(String[] args) {
+        new Test();
+    }
+}
